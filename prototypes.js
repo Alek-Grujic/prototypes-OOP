1de2bb9356ce63c639ac8e1947b8a9c3d97b524d
Alek-Grujic <alek.grujic001@outlook.com>
2025-06-21 12:49:43 +0200

    Add computer and laptop example using prototype inheritance

--- prototypes.js
+++ prototypes.js
@@ -1,32 +1,64 @@
 
-function createAnimal(name) {
+// function createAnimal(name) {
+//     return {
+//         name: name,
+//         speak() {
+//             console.log(`Animal sound`);
+//         }
+//     };
+// }
+
+// function createCat(name, color) {
+//     const cat = {
+//         color: color,
+//         meow() {
+//             console.log(`${name} says moew!`);
+//         },
+//         toString() {
+//             return (`Cat: name - ${name}, color - ${color}`);
+//         }
+
+//     };
+//     cat.__proto__ = createAnimal(name);
+
+//     return cat;
+// }
+
+// const c = createCat('Catty', 'black');
+
+// c.meow();
+// c.speak();
+
+// console.log(c.toString());
+
+
+
+function createComputer(brand) {
     return {
-        name: name,
-        speak() {
-            console.log(`Animal sound`);
+        brand: brand,
+        info() {
+            console.log(`Computer brand: ${brand}`);
         }
-    };
+    }
 }
 
-function createCat(name, color) {
-    const cat = {
-        color: color,
-        meow() {
-            console.log(`${name} says moew!`);
+function createLaptop(brand, weight) {
+    const laptop = {
+        weight: weight,
+        carry() {
+            console.log(`Carrying ${brand} laptop that weighs ${weight}kg`);
         },
         toString() {
-            return (`Cat: name - ${name}, color - ${color}`);
+            return (`Laptop: ${brand}, ${weight}kg`)
         }
+    }
+    laptop.__proto__ = createComputer(brand);
 
-    };
-    cat.__proto__ = createAnimal(name);
-
-    return cat;
+    return laptop;
 }
 
-const c = createCat('Catty', 'black');
+const I = createLaptop(`Asus`, 7);
 
-c.meow();
-c.speak();
-
-console.log(c.toString());
+I.carry();
+I.info();
+console.log(I.toString());
